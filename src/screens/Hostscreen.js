0c4a197bf4b3a90c6cf3b404233173e41e6d2f6d
GuptaTanisha <tanishagupta123456789tg@gmail.com>
2022-03-07 12:06:33 +0530

    Add cards for host

--- src/screens/Hostscreen.js
+++ src/screens/Hostscreen.js
@@ -20,8 +20,8 @@ const Hostscreen = () => {
    },[dispatch])
 
     const hostsFromState = useSelector((state) => state.hosts);
-    const {hosts} = hostsFromState;
-    // console.log("me",{hosts.length});
+    var {hosts} = hostsFromState;
+    console.log({hosts});
 
     return(
         <>
@@ -29,34 +29,19 @@ const Hostscreen = () => {
         <Header home={false} />
         <Grid container spacing={3} style={{ width: '100%'}}>
     {
-    hosts.length ? (hosts.map((host,i) => (
-    <Grid item xs={12} md={3} key={station._id} >
-        <Card elevation={6} key={station._id} className={classes.root}>
-        <CardMedia style={{ height: 250}} title={host.Name} />
+    hosts?.length ? (hosts.map((host,i) => (
+    <Grid item xs={12} md={3} key={host._id} >
+        <Card sx={{ minWidth: 275 }} elevation={6} key={host._id} >
               <CardContent>
-              <Typography variant="h6">Id: {host.PhoneNumber}</Typography>
-              <Typography variant="h6">Id: {host.Email}</Typography>
-              <Typography variant="h6">Id: {host.City}</Typography>
-          {/* <Divider variant="middle" />
-          
-          <Grid container spacing={2}>
-          <Grid item md={1}>
-          <LocationOnIcon />
-          </Grid>
-          <Grid item  md={10}>
-          <Typography color="text.secondary" variant="body2">{station.address}</Typography>
-          <Typography color="text.secondary" variant="body2">Lat : {station.lat}, Long: {station.long}</Typography>
-          <Typography color="text.primary" variant="body1">{station.country}</Typography>
-          </Grid>
-          </Grid>
-          <Divider variant="middle" /> */}
+              <Typography variant="body2" color="text.secondary">Host's Unique Id: {host._id} </Typography>
+              <br />
+               <Typography align="center" children="node" color="textSecondary" gutterBottom><i class="fa-solid fa-phone"></i> {host.PhoneNumber}</Typography>
+               <br />
+              <Typography align="center" variant="h4" component="div"><i class="fa-solid fa-user"></i>{" "}{host.Name}</Typography>
+              <br />
+              <Typography align="center" color="textSecondary" variant="h6"><i class="fa-solid fa-city"></i>{" "}{host.City}</Typography>
+              <Typography align="center" variant="body2"><i class="fa-solid fa-envelope"></i> {" "}{host.Email} <br /> </Typography>
               </CardContent>
-    
-          {/* <CardActions >
-          <Button className={classes.button} variant="contained" color="primary" size="small" onClick={() => handleClick(station.stationId)}>
-                  BOOK
-            </Button>
-          </CardActions> */}
      </Card>
     </Grid>
     ))) : (<Typography variant="h4">Nothing to Show</Typography>)
